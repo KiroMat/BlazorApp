c7544dde2d38e91f62d1421267f6b619799bb55c
Jakub Garbiński <kiromat@hotmail.com>
2022-10-12 20:08:42 +0200

    Init Web API and Webassembly Blazor App

---
+++ DataApi/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFront", policy =>
+    {
+        policy.WithOrigins("https://localhost:7193", "http://localhost:5193");
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowFront");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
