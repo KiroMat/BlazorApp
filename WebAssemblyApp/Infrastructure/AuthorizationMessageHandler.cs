22941a8c0a8abd8088593469b09aea7624307dfa
Jakub Garbiński <kiromat@hotmail.com>
2022-10-13 19:37:24 +0200

    MudBlazor

---
+++ WebAssemblyApp/Infrastructure/AuthorizationMessageHandler.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.LocalStorage;
+using System.Net.Http.Headers;
+
+namespace WebAssemblyApp.Infrastructure
+{
+    public class AuthorizationMessageHandler : DelegatingHandler
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        private const string AccessTokenKey = "acces_token";
+
+        public AuthorizationMessageHandler(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if(await _localStorage.ContainKeyAsync(AccessTokenKey))
+            {
+                var token = await _localStorage.GetItemAsStringAsync(AccessTokenKey);
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+            Console.WriteLine("Test call to API");
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
--- WebAssemblyApp/Program.cs
+++ WebAssemblyApp/Program.cs
@@ -1,11 +1,21 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using WebAssemblyApp;
+using WebAssemblyApp.Infrastructure;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("ApiUrl")) });
+builder.Services.AddTransient<AuthorizationMessageHandler>();
+builder.Services.AddHttpClient("WebApi", client => 
+{
+    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("ApiUrl"));
+}).AddHttpMessageHandler<AuthorizationMessageHandler>();
+builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("WebApi"));
 
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 await builder.Build().RunAsync();
